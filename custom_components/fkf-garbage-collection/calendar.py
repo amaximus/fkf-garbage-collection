7dbe91c04d54ee10150a9f8a213c17b5e4f416d8
attila <atticus.rationalis@gmail.com>
2021-02-27 01:01:14 +0100

    Fix calendar after adding green support

--- custom_components/fkf-garbage-collection/calendar.py
+++ custom_components/fkf-garbage-collection/calendar.py
@@ -68,12 +68,12 @@ class EntitiesCalendarData:
         self._hass = hass
         self.entities = []
         self._translation = {
-            "hu": { "both": "kommunális, szelektív",
-                    "communal": "kommunális",
+            "hu": { "communal": "kommunális",
+                    "green": "zöld",
                     "selective": "szelektív"
                   },
-            "en": { "both": "communal, selective",
-                    "communal": "communal",
+            "en": { "communal": "communal",
+                    "green": "green",
                     "selective": "selective"
                   }
        }
@@ -88,12 +88,21 @@ class EntitiesCalendarData:
         if entity_id in self.entities:
             self.entities.remove(entity_id)
 
+    def _split_and_translate(self, lng, gstr):
+        gtranslated = ""
+        garbagelist = gstr.split("_")
+        for g in garbagelist:
+            gtranslated += self._translation[lng][g] + ","
+        gtranslated = gtranslated[:-1]
+        return gtranslated
+
     async def async_get_events(self, hass, start_datetime, end_datetime):
         """Get all tasks in a specific time frame."""
         events = []
         startdates = {}
         garbages = {}
         calendar_lang = "en"
+        friendly_name = ""
         if SENSOR_PLATFORM not in hass.data[DOMAIN]:
             return events
         #start_date = start_datetime.date()
@@ -102,7 +111,6 @@ class EntitiesCalendarData:
             if entity not in hass.data[DOMAIN][SENSOR_PLATFORM]:
                 continue
             attributes = self._hass.states.get(entity).attributes
-            friendly_name = attributes['friendly_name']
             for key in attributes:
                 x = re.search('^date', key)
                 if x is not None:
@@ -114,15 +122,18 @@ class EntitiesCalendarData:
                     garbages[idx] = attributes[key]
                 if key == 'calendar_lang':
                     calendar_lang = attributes[key]
+                if key == 'friendly_name':
+                    friendly_name = attributes[key]
 
             i = 0
             while i < len(startdates):
                 if startdates[str(i)] is not None:
                     end = startdates[str(i)] + timedelta(days=1)
                     if calendar_lang in self._translation:
-                      gtype = self._translation[calendar_lang][garbages[str(i)]]
+                      gtype = self._split_and_translate(calendar_lang, garbages[str(i)])
                     else:
-                      gtype = self._translation["en"][garbages[str(i)]]
+                      gtype = self._split_and_translate("en", garbages[str(i)])
+                    _LOGGER.debug("async_get_events: %s s: %s, e: %s, type: %s", friendly_name, startdates[str(i)].strftime("%Y.%m.%d"), end.strftime("%Y.%m.%d"),gtype)
 
                     event = {
                         "uid": entity,
@@ -140,13 +151,13 @@ class EntitiesCalendarData:
         """Get the latest data."""
         next_dates = {}
         garbages = {}
-        calendar_lang = ""
-        i = 0
+        calendar_lang = "en"
+        friendly_name = ""
+        #i = 0
         for entity in self.entities:
           if entity not in self._hass.data[DOMAIN][SENSOR_PLATFORM]:
             continue
           attributes = self._hass.states.get(entity).attributes
-          friendly_name = attributes['friendly_name']
           for key in attributes:
             x = re.search('^date', key)
             if x is not None:
@@ -160,15 +171,19 @@ class EntitiesCalendarData:
 
             if key == 'calendar_lang':
               calendar_lang = attributes[key]
+            if key == 'friendly_name':
+              friendly_name = attributes[key]
 
         if len(next_dates) > 0:
           idx = min(next_dates.keys(), key=(lambda k: next_dates[k]))
           start = next_dates[str(idx)]
           end = start + timedelta(days=1)
           if calendar_lang in self._translation:
-            name = self._translation[calendar_lang][garbages[str(i)]]
+            name = self._split_and_translate(calendar_lang, garbages[str(idx)])
           else:
-            name = self._translation["en"][garbages[str(i)]]
+            name = self._split_and_translate("en", garbages[str(idx)])
+
+          _LOGGER.debug("async_update: %s s: %s, e: %s, type: %s", friendly_name, start.strftime("%Y.%m.%d"), end.strftime("%Y.%m.%d"),name)
 
           self.event = {
             "uid": str(idx),
